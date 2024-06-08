ddec0b809fe7bf155ca25962950334ff23069553
Andres Felipe Rios Bolivar <sql.programador29@opa.com.co>
2024-06-08 18:32:01 -0500

    Remove credentials and provider from local

--- provider.tf
+++ provider.tf
@@ -11,16 +11,16 @@ terraform {
     key                         = "githubrunners/githubrunners.tfstate"
     encrypt                     = true
     region                      = "us-east-1"
-    shared_credentials_files    = ["C:\\Users\\sql.programador29\\.aws\\credentials"]
-    profile                     = "default"
+    #shared_credentials_files    = ["C:\\Users\\sql.programador29\\.aws\\credentials"]
+    #profile                     = "default"
     dynamodb_table              = "tfstate_githubrunners"
   }
 }
 
 # AWS Provider
 provider "aws" {
-  shared_config_files      = ["C:\\Users\\sql.programador29\\.aws\\config"]
-  shared_credentials_files = ["C:\\Users\\sql.programador29\\.aws\\credentials"]
-  profile                  = "default"
+  #shared_config_files      = ["C:\\Users\\sql.programador29\\.aws\\config"]
+  #shared_credentials_files = ["C:\\Users\\sql.programador29\\.aws\\credentials"]
+  #profile                  = "default"
   region                   = "us-east-1" 
 }
